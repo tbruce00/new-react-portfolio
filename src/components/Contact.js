6fdae5e9323652b772e23f40c37f48f4a0e0f8eb
Trevor Bruce <tbnyk03@gmail.com>
2021-12-13 10:24:49 -0700

    Push to git

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import Form from '../components/Form';
+import Form from './Form';
 import '../assets/contact.css';
 
 export default function Contact() {
     return (
-        <div className="container">
+        <div id="contact" className="container">
         <div className="mx-auto mt-auto p-auto">
         <h1 className="text-center">contact me</h1>
         <Form />
